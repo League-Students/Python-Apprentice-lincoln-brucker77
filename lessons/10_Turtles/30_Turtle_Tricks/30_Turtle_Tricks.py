6c42e3626db76ff63e2964d35509b438ac2e6253
League Student <student@jointheleague.org>
2026-07-02 13:21:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/30_Turtle_Tricks/30_Turtle_Tricks.py
+++ lessons/10_Turtles/30_Turtle_Tricks/30_Turtle_Tricks.py
@@ -25,7 +25,7 @@ for i in range (4):
     left = 90
     turtle.forward (f)
     turtle.left(left)
-turtle.circle(200)
+turtle.circle(150)
 
 turtle.exitonclick()                    # Close the window when we click on it
 
