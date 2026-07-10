d76c40deb88930e1ca18eb2c8974ea68a2eb87d9
League Student <student@jointheleague.org>
2026-07-10 12:53:11 -0700

    Automated commit

--- lessons/50_Projects/fnaf.py
+++ lessons/50_Projects/fnaf.py
@@ -5,7 +5,7 @@ tina = turtle.Turtle()
 screen = turtle.Screen()
 screen.setup(500,500)
 
-cam_colors = ["red","black","white","blue","green"]
+cam_colors = ["red","black","purple","blue","green"]
 
 def open_cam_1():
     print("cam 1 open")
